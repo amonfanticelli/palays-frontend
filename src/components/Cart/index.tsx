e65728ac5b1cf4a15504a3ff5e01e685218d09e5
Amon <amon_fanticelli@hotmail.com>
2023-09-17 14:32:52 -0300

    style: cart mobile updated and ready

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -40,90 +40,96 @@ export default function Cart() {
   }
 
   return (
-    <section className="flex flex-col bg-white fixed w-[480px] h-full max-h-[900px] z-10 top-0 right-0 bottom-0 shadow-[-4px_0_30px_0_rgba(0,0,0,0.80)] ">
+    <section className="flex flex-col bg-white fixed w-[480px] h-full max-h-[900px] z-10 top-0 right-0 bottom-0 shadow-[-4px_0_30px_0_rgba(0,0,0,0.80)] px-4 max-[768px]:w-full overflow-scroll">
       <div className="flex justify-end my-6 mr-6">
         <button onClick={() => setIsCartOpen(false)}>
           <X weight="bold" size={24} />
         </button>
       </div>
 
-      <h2 className="ml-10 mb-8 font-helvetica font-bold text-xl">
+      <h2 className="ml-10 mb-8 font-helvetica font-bold text-xl max-[768px]:ml-0">
         Carrinho de Compras
       </h2>
-      <ul className="mx-10 flex flex-col gap-6 min-h-[472px] max-h-[472px] overflow-auto relative">
-        {!cartItems.length ? (
-          <span className="font font-helvetica font-bold self-center absolute bottom-[210px] ">
-            Seu carrinho está vazio
-          </span>
-        ) : (
-          cartItems.map(({ prod, quantity }) => (
-            <li key={prod.id} className="flex justify-between">
-              <Image
-                src={prod.imageUrl}
-                width={100}
-                height={100}
-                alt="imagem do produto no carrinho"
-              ></Image>
-              <div className="flex flex-col gap-2 w-full max-w-[280px]">
-                <span className="font-normal text-lg text-gray-500 font-helvetica">
-                  {prod.name}
-                </span>
-                <div className="flex items-center justify-between">
-                  <span className="font-helvetica font-bold text-lg">
-                    {new Intl.NumberFormat("pt-BR", {
+      <div className="h-full flex flex-col justify-between">
+        <ul className="mx-10 flex flex-col gap-6  max-h-[472px] overflow-auto relative max-[768px]:mx-0 max-[768px]:max-h-[372px]">
+          {!cartItems.length ? (
+            <span className="font font-helvetica font-bold self-center absolute bottom-[210px] ">
+              Seu carrinho está vazio
+            </span>
+          ) : (
+            cartItems.map(({ prod, quantity }) => (
+              <li key={prod.id} className="flex justify-between">
+                <Image
+                  src={prod.imageUrl}
+                  width={100}
+                  height={100}
+                  alt="imagem do produto no carrinho"
+                ></Image>
+                <div className="flex flex-col gap-2 w-full max-w-[280px] ml-2">
+                  <span className="font-normal text-lg text-gray-500 font-helvetica">
+                    {prod.name}
+                  </span>
+                  <div className="flex items-center justify-between">
+                    <span className="font-helvetica font-bold text-lg">
+                      {new Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      }).format(prod.numberPrice * quantity)}
+                    </span>
+                    <div className="flex border border-gray-400 items-center gap-3 px-2 h-11">
+                      <button onClick={() => minusRemoveFromCart(prod)}>
+                        <Minus size={16} weight={"regular"} />
+                      </button>
+                      <span>{quantity}</span>
+                      <button onClick={() => addToCart(prod)}>
+                        <Plus size={16} weight={"regular"} />
+                      </button>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => removeCartItem(prod.id)}
+                    className="flex font-helvetica font-bold text-base hover:text-red-600 transition duration-300 "
+                  >
+                    Remover
+                  </button>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+        <div className="flex flex-col">
+          <div className="mx-10 mb-[55px] mt-[68px] max-[768px]:mx-0 max-[768px]:mb-4 max-[768px]:mt-4">
+            <div className="flex justify-between">
+              <span className="text-gray-500 font-helvetica text-base font-normal">
+                Quantidade
+              </span>
+              <span className="text-gray-500 font-helvetica font-normal text-lg">
+                {cartItems.length} {cartItems.length === 1 ? "Item" : "Items"}
+              </span>
+            </div>
+            <div className="flex justify-between">
+              <span className="font-bold font-helvetica text-lg">
+                Valor Total
+              </span>
+              <span className="font-bold font-helvetica text-lg">
+                {cartItems.length <= 1
+                  ? cartTotalFormatted
+                  : new Intl.NumberFormat("pt-BR", {
                       style: "currency",
                       currency: "BRL",
-                    }).format(prod.numberPrice * quantity)}
-                  </span>
-                  <div className="flex border border-gray-400 items-center gap-3 px-2 h-11">
-                    <button onClick={() => minusRemoveFromCart(prod)}>
-                      <Minus size={16} weight={"regular"} />
-                    </button>
-                    <span>{quantity}</span>
-                    <button onClick={() => addToCart(prod)}>
-                      <Plus size={16} weight={"regular"} />
-                    </button>
-                  </div>
-                </div>
-                <button
-                  onClick={() => removeCartItem(prod.id)}
-                  className="flex font-helvetica font-bold text-base hover:text-red-600 transition duration-300"
-                >
-                  Remover
-                </button>
-              </div>
-            </li>
-          ))
-        )}
-      </ul>
-      <div className="mx-10 mb-[55px] mt-[68px]">
-        <div className="flex justify-between">
-          <span className="text-gray-500 font-helvetica text-base font-normal">
-            Quantidade
-          </span>
-          <span className="text-gray-500 font-helvetica font-normal text-lg">
-            {cartItems.length} {cartItems.length === 1 ? "Item" : "Items"}
-          </span>
-        </div>
-        <div className="flex justify-between">
-          <span className="font-bold font-helvetica text-lg">Valor Total</span>
-          <span className="font-bold font-helvetica text-lg">
-            {cartItems.length <= 1
-              ? cartTotalFormatted
-              : new Intl.NumberFormat("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                }).format(cartTotal)}
-          </span>
+                    }).format(cartTotal)}
+              </span>
+            </div>
+          </div>
+          <button
+            disabled={isCreatingCheckoutSession}
+            onClick={handleCheckout}
+            className=" self-center w-full max-w-[384px] min-h-[45px] text-gray-50 border border-black bg-black disabled:opacity-60 disabled:cursor-not-allowed mb-4 max-[768px]:max-w-none"
+          >
+            Finalizar Compra
+          </button>
         </div>
       </div>
-      <button
-        disabled={isCreatingCheckoutSession}
-        onClick={handleCheckout}
-        className="self-center w-[384px] min-h-[45px] text-gray-50 border border-black bg-black disabled:opacity-60 disabled:cursor-not-allowed mb-4"
-      >
-        Finalizar Compra
-      </button>
     </section>
   );
 }
